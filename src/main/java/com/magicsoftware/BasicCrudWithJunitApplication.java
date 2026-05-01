537082444cdb0fdddba5e80129009f56476c15fb
P J Mugilan <pjmugilan@gmail.com>
2026-05-01 19:13:09 +0530

    Initial spring boot project setup

---
+++ src/main/java/com/magicsoftware/BasicCrudWithJunitApplication.java
@@ -0,0 +1,13 @@
+package com.magicsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudWithJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCrudWithJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magicsoftware/BasicCrudWithJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudWithJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
